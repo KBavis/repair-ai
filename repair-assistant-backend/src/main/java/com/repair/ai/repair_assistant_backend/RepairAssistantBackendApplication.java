89f4eb910a936cc6be174e0f325f8e7e1be611b8
Kellen Bavis <kellenrbavis@gmail.com>
2025-07-06 01:39:06 -0400

    init

---
+++ repair-assistant-backend/src/main/java/com/repair/ai/repair_assistant_backend/RepairAssistantBackendApplication.java
@@ -0,0 +1,13 @@
+package com.repair.ai.repair_assistant_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepairAssistantBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepairAssistantBackendApplication.class, args);
+	}
+
+}
---
+++ repair-assistant-backend/src/test/java/com/repair/ai/repair_assistant_backend/RepairAssistantBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.repair.ai.repair_assistant_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepairAssistantBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
