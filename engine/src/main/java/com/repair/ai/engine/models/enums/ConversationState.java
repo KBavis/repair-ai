c1c6daeee56bc6f03b19a029d2e6e9d07636b688
Kellen Bavis <kellenrbavis@gmail.com>
2025-07-09 01:10:38 -0400

    feat: enum for tracking conversation state

---
+++ engine/src/main/java/com/repair/ai/engine/models/enums/ConversationState.java
@@ -0,0 +1,8 @@
+package com.repair.ai.engine.models.enums;
+
+public enum ConversationState {
+    INITIAL_ASSESSMENT,
+    SAFETY_CHECK,
+    DIAGNOSTIC_QUESTION
+    //TODO: Add remainder states
+}
