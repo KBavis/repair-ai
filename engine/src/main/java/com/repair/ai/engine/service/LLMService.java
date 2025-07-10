1311464ae54512567f8b11c88517dfb36e738f21
Kellen Bavis <kellenrbavis@gmail.com>
2025-07-09 23:51:29 -0400

    feat: make model interaction to generic to reuse across different conversation states

--- engine/src/main/java/com/repair/ai/engine/service/LLMService.java
+++ engine/src/main/java/com/repair/ai/engine/service/LLMService.java
@@ -2,14 +2,12 @@ package com.repair.ai.engine.service;
 
 import com.openai.client.OpenAIClient;
 import com.openai.models.ChatModel;
-import com.openai.models.responses.*;
-import com.repair.ai.engine.models.wrapper.Repair;
+import com.openai.models.responses.ResponseCreateParams;
+import com.openai.models.responses.StructuredResponse;
+import com.openai.models.responses.StructuredResponseCreateParams;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.Optional;
 
 @Log4j2
 @Service
@@ -18,28 +16,14 @@ public class LLMService {
     @Autowired
     private OpenAIClient openAIClient;
 
-    public Repair testingApiCallToOpenAi() {
+    public StructuredResponse<?> callOpenAiModel(String message, Class<?> klass) {
+        log.info("Calling OpenAI LLM will with user defined message: [{}]", message);
 
-        String testInput = "My kitchen sink won't drain properly. The water goes down very slowly. Please diagnose the issue and tell me the repair.";
-
-        StructuredResponseCreateParams<Repair> params = ResponseCreateParams.builder()
-                .input(testInput)
+        StructuredResponseCreateParams<?> params = ResponseCreateParams.builder()
+                .input(message)
                 .model(ChatModel.GPT_4O)
-                .text(Repair.class)
+                .text(klass)
                 .build();
-
-        StructuredResponse<Repair> repair = openAIClient.responses().create(params);
-        Repair suggestRepair = repair.output().stream()
-                        .map(StructuredResponseOutputItem::message)
-                        .filter(Optional::isPresent)
-                        .map(Optional::get)
-                        .map(StructuredResponseOutputMessage::content)
-                        .map(List::getFirst)
-                        .map(StructuredResponseOutputMessage.Content::asOutputText)
-                        .findFirst().orElse(null);
-
-
-        log.info(suggestRepair);
-        return suggestRepair;
+        return openAIClient.responses().create(params);
     }
 }
