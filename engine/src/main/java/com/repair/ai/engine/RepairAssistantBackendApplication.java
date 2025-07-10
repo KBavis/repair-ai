895ed4de976ed84e89ba260289b3bb46b77fdf85
Kellen Bavis <kellenrbavis@gmail.com>
2025-07-10 00:03:25 -0400

    feat: add necessary base packages to main

--- engine/src/main/java/com/repair/ai/engine/RepairAssistantBackendApplication.java
+++ engine/src/main/java/com/repair/ai/engine/RepairAssistantBackendApplication.java
@@ -2,8 +2,12 @@ package com.repair.ai.engine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.repair.ai.engine.repository")
+@EntityScan(basePackages = "com.repair.ai.engine.models.entity")
 public class RepairAssistantBackendApplication {
 
 	public static void main(String[] args) {
